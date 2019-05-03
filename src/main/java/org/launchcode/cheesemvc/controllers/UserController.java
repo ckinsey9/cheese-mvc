4f1604e6853c15141974a8ba29eff2527e6c5e06
ckinsey9 <48170234+ckinsey9@users.noreply.github.com>
2019-05-02 20:25:40 -0500

    Added User class and rendered the Register form.

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -1,10 +1,23 @@
 package org.launchcode.cheesemvc.controllers;
 
 
+import org.launchcode.cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+
+    @RequestMapping("add")
+    public String add(Model model) {
+        String verify_error = "";
+
+        model.addAttribute("title", "Register User");
+        model.addAttribute(new User());
+        model.addAttribute("verify_error", verify_error);
+        return "user/add";
+    }
 }
